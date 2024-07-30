6ea1dc790a540f2ca29f1f3ebb18d46ab2cd3e58
Blynxy Furyfur <blynxy@cathead.media>
2024-07-29 20:13:02 -0500

    sync proj folders rsync scripts

---
+++ backup/pull_proj.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsync -a --delete --quiet lynx-nas:/home/lynx/nas/dataOwO/mcgtn /home/lynx/mcgtn_proj
---
+++ backup/push_proj.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsync -a --delete --quiet /home/lynx/mcgtn_proj lynx-nas:/home/lynx/nas/dataOwO/mcgtn
